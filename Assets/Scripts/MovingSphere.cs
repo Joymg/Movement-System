07229bfe64588fa351524efadcf788341ef46995
Joymg <josemg98and@gmail.com>
2021-11-12 13:27:54 +0100

    Normalizing Input Vector

--- Assets/Scripts/MovingSphere.cs
+++ Assets/Scripts/MovingSphere.cs
@@ -23,8 +23,6 @@ public class MovingSphere : MonoBehaviour
         playerInput.x = Input.GetAxis("Horizontal");
         playerInput.y = Input.GetAxis("Vertical");
         playerInput.Normalize();
-
-        playerInput = Vector2.ClampMagnitude(playerInput, 1f);
         transform.localPosition = new Vector3(playerInput.x, 0.5f, playerInput.y);
 
     }
