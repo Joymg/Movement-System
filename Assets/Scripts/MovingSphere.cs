a3d8154d3d2b8d08cebef48248fbb6fa1d072dae
Joymg <josemg98and@gmail.com>
2021-11-12 21:42:44 +0100

    Air jumps

--- Assets/Scripts/MovingSphere.cs
+++ Assets/Scripts/MovingSphere.cs
@@ -15,7 +15,11 @@ public class MovingSphere : MonoBehaviour
     [SerializeField, Range(0f, 10f)]
     float jumpHeight = 2f;
 
+    [SerializeField, Range(0, 5)]
+    int maxAirJumps = 0;
+
     Vector3 velocity, desiredVelocity;
+    int jumpPhase;
 
     Rigidbody body;
 
@@ -43,8 +47,7 @@ public class MovingSphere : MonoBehaviour
     private void FixedUpdate()
     {
 
-
-        velocity = body.velocity;
+        UpdateState();
         //find maximum speed change this update
         float maxSpeedChange = maxAcceleration * Time.fixedDeltaTime;
         velocity.x =
@@ -64,10 +67,21 @@ public class MovingSphere : MonoBehaviour
 
     }
 
-    void Jump()
+    private void UpdateState()
     {
+
+        velocity = body.velocity;
         if (isGrounded)
         {
+            jumpPhase = 0;
+        }
+    }
+
+    void Jump()
+    {
+        if (isGrounded || jumpPhase < maxAirJumps)
+        {
+            jumpPhase += 1;
             //Using gravity to calculate jump force
             velocity.y += Mathf.Sqrt(-2f * Physics.gravity.y * jumpHeight);
         }
