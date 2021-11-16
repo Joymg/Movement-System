223c353bae82e3b76cb9e0961cafca8a770fb4b8
Joymg <josemg98and@gmail.com>
2021-11-17 00:19:22 +0100

    Center the focus

--- Assets/Scripts/OrbitalCamera.cs
+++ Assets/Scripts/OrbitalCamera.cs
@@ -23,6 +23,12 @@ public class OrbitalCamera : MonoBehaviour
     [SerializeField, Min(0f)]
     float focusRadius = 1f;
 
+    /// <summary>
+    /// Factor in charge of centering the focus 
+    /// </summary>
+    [SerializeField, Range(0f, 1f)]
+    float focusCentering = 0.5f;
+
     Vector3 focusPoint;
 
     private void Awake()
@@ -46,11 +52,20 @@ public class OrbitalCamera : MonoBehaviour
         {
             //if the dist between target and current focus is greater than the radius
             float distance = Vector3.Distance(targetPoint, focusPoint);
+            float t = 1f;
+            //if the distance is big enougth tyo be noticeable and focus centering is positive
+            if (distance > 0.01f && focusCentering > 0f)
+            {
+                //interpolate between current focus and target using (1-focusCentering)^deltatime as interpolator
+                t = Mathf.Pow(1f - focusCentering, Time.unscaledDeltaTime);
+            }
+
             if (distance > focusRadius)
             {
                 //pull focus towards target
-                focusPoint = Vector3.Lerp(targetPoint,focusPoint,focusRadius/distance);
+                t = Mathf.Min(t, focusRadius / distance);
             }
+            focusPoint = Vector3.Lerp(targetPoint, focusPoint, t);
         }
         //otherwise set focus point to targer
         else
