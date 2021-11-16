6fce6630f9ca66e4d3af97ec1f3a4c746530f508
Joymg <josemg98and@gmail.com>
2021-11-16 23:51:00 +0100

    Maintaining Relative Position

--- Assets/Scripts/OrbitalCamera.cs
+++ Assets/Scripts/OrbitalCamera.cs
@@ -5,15 +5,23 @@ using UnityEngine;
 [RequireComponent(typeof(Camera))]
 public class OrbitalCamera : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    /// <summary>
+    /// Target followed by the camera
+    /// </summary>
+    [SerializeField]
+    Transform target = default;
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Distance the camera will follow the target
+    /// </summary>
+    [SerializeField, Range(1f, 20f)]
+    float distance = 5f;
+
+    //using late updat in case anything moves the target in update
+    private void LateUpdate()
     {
-        
+        Vector3 targetPosition = target.position;
+        Vector3 lookDirection = transform.forward;
+        transform.localPosition = targetPosition - lookDirection * distance;
     }
 }
