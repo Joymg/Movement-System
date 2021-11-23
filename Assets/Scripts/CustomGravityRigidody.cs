1665f4f57ce27a135b12711beb262ae304b5463c
Joymg <josemg98and@gmail.com>
2021-11-23 12:40:51 +0100

    Making other staying awake if needed

--- Assets/Scripts/CustomGravityRigidody.cs
+++ Assets/Scripts/CustomGravityRigidody.cs
@@ -16,6 +16,13 @@ public class CustomGravityRigidody : MonoBehaviour
     /// </summary>
     float floatDelay;
 
+    /// <summary>
+    /// Making configurable if a body is allowed to float so it can go to sleep
+    /// </summary>
+    [SerializeField]
+    bool floatToSleep = false;
+
+
     private void Awake()
     {
         body = GetComponent<Rigidbody>();
@@ -24,28 +31,31 @@ public class CustomGravityRigidody : MonoBehaviour
 
     private void FixedUpdate()
     {
-        //if it is sleeping do not disturb it
-        if (body.IsSleeping())
+        if (floatToSleep)
         {
-            gameObject.GetComponent<Renderer>().material.color = Color.red;
-            floatDelay = 0f;
-            return;
-        }
-
-        //assuming it never goes to sleep if its velocity its really small, gravity is not applied
-        if (body.velocity.sqrMagnitude < 0.0001f)
-        {
-            floatDelay += Time.deltaTime;
-            if (floatDelay >= 1f)
+            //if it is sleeping do not disturb it
+            if (body.IsSleeping())
             {
-                gameObject.GetComponent<Renderer>().material.color = Color.yellow;
+                gameObject.GetComponent<Renderer>().material.color = Color.red;
+                floatDelay = 0f;
                 return;
             }
-        }
-        else
-        {
-            gameObject.GetComponent<Renderer>().material.color = Color.green;
-            floatDelay = 0f;
+
+            //assuming it never goes to sleep if its velocity its really small, gravity is not applied
+            if (body.velocity.sqrMagnitude < 0.0001f)
+            {
+                floatDelay += Time.deltaTime;
+                if (floatDelay >= 1f)
+                {
+                    gameObject.GetComponent<Renderer>().material.color = Color.yellow;
+                    return;
+                }
+            }
+            else
+            {
+                gameObject.GetComponent<Renderer>().material.color = Color.green;
+                floatDelay = 0f;
+            }
         }
 
         body.AddForce(CustomGravity.GetGravity(body.position),ForceMode.Acceleration);
