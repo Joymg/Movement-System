211e88beb94009cdd43624dcb832ecf10f443ff0
Joymg <josemg98and@gmail.com>
2021-11-24 11:44:03 +0100

    Gravity Range

--- Assets/Scripts/GravityPlane.cs
+++ Assets/Scripts/GravityPlane.cs
@@ -5,9 +5,36 @@ public class GravityPlane : GravitySource
     [SerializeField]
     float gravity = 9.81f;
 
+    [SerializeField]
+    float range = 1f;
+
     public override Vector3 GetGravity(Vector3 position)
     {
         Vector3 up = transform.up;
+        float distance = Vector3.Dot(up, position - transform.position);
+        if (distance > range)
+        {
+            return Vector3.zero;
+        }
         return -gravity * up;
     }
+
+#if UNITY_EDITOR
+
+    private void OnDrawGizmos()
+    {
+        Vector3 scale = transform.localScale;
+        scale.y = range;
+        Gizmos.matrix = Matrix4x4.TRS(transform.position,transform.rotation,scale);
+        Vector3 size = new Vector3(1f, 0f, 1f);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(Vector3.zero, size);
+        if (range >0 )
+        {
+            Gizmos.color = Color.cyan;
+            Gizmos.DrawWireCube(Vector3.up, size);
+        }
+
+    }
+#endif
 }
