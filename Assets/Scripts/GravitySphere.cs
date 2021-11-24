aba567445b1a09f6818f22bb526046007736eb88
Joymg <josemg98and@gmail.com>
2021-11-24 12:55:53 +0100

    Applying gravity from spheres

--- Assets/Scripts/GravitySphere.cs
+++ Assets/Scripts/GravitySphere.cs
@@ -9,6 +9,11 @@ public class GravitySphere : GravitySource
 	[SerializeField, Min(0f)]
 	float outerRadius = 10f, outerFalloffRadius = 15f;
 
+    /// <summary>
+    /// Falloff range
+    /// </summary>
+    float outerFalloffFactor;
+
     private void Awake()
     {
         OnValidate();
@@ -17,6 +22,32 @@ public class GravitySphere : GravitySource
     private void OnValidate()
     {
         outerFalloffRadius = Mathf.Max(outerFalloffRadius,outerRadius);
+        outerFalloffFactor = 1f / (outerFalloffRadius - outerRadius);
+    }
+
+    
+    public override Vector3 GetGravity(Vector3 position)
+    {
+        //finds the vector pointing from postition to sphere's center
+        Vector3 vector = transform.position - position;
+        //distance is vector's magnitude
+        float distance = vector.magnitude;
+
+        //if distance is greater the oter fallof radius gravity is not applied
+        if (distance > outerFalloffRadius)
+        {
+            return Vector3.zero;
+        }
+        //otherwise is the vector scaled by gravity
+        float g = gravity/distance;
+
+        //reducing gravity linearly between radius and radius falloff
+        if (distance > outerRadius)
+        {
+            //distance beyond the outer radius divided by falloff range
+            g *= 1f - (distance - outerRadius) * outerFalloffFactor;
+        }
+        return g * vector;
     }
 
 #if UNITY_EDITOR
