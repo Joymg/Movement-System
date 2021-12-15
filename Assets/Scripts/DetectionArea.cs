f9c9431bbe48c45e9a00bc87368ca0f98e07c78e
Joymg <josemg98and@gmail.com>
2021-12-15 17:23:57 +0100

    Detection Areas detects changes on first enter and last exit only

--- Assets/Scripts/DetectionArea.cs
+++ Assets/Scripts/DetectionArea.cs
@@ -1,19 +1,33 @@
 
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class DetectionArea : MonoBehaviour
 {
     [SerializeField]
-    UnityEvent onEnter = default, onExit = default;
+    UnityEvent onFirstEnter = default, onLastExit = default;
+
+    //List of colliders used to check if there is any colliders in the area
+    List<Collider> colliders = new List<Collider>();
 
     private void OnTriggerEnter(Collider other)
     {
-        onEnter.Invoke();
+        //only invoke the event if there is no item in the area
+        if (colliders.Count == 0)
+        {
+            onFirstEnter.Invoke();
+        }
+        //adds the collider to the list
+        colliders.Add(other);
     }
 
     private void OnTriggerExit(Collider other)
     {
-        onExit.Invoke();
+        //removes collider from the list and if it is empty invoke exit event
+        if (colliders.Remove(other) && colliders.Count==0)
+        {
+            onLastExit.Invoke();
+        }
     }
 }
